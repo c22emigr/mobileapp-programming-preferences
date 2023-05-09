974be22dab09803b1e5e717237089bc4382059c0
c22emigr <c22emigr@student.his.se>
2023-05-09 13:24:56 +0200

    Added second activity with new widgets. Also added code in main activity so i can save in textview

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
     }
 
+    public void savePref(View v){
+        // Get the text
+        EditText newPrefText=new EditText(this);
+        newPrefText=(EditText)findViewById(R.id.settingseditview);
+
+        // Store the new preference
+        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+
+        // Display the new preference
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
+        // Clear the EditText
+        newPrefText.setText("");
+    }
 }
